47b4183372cc3fb06f3e127b3e9d376171410b8a
roboticistYan <yc3240@columbia.edu>
2017-11-15 17:52:30 -0500

    feat: Minimum deployment of ROS + ZED
    The cmake file is designed to include basic requirement from both ROS
    and ZED libraries.

---
+++ venom_zed/src/example.cpp
@@ -0,0 +1,10 @@
+#include <sl/Camera.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <ros/ros.h>
+
+int main(int argc, char** argv){
+  sl::Camera zed;
+  cv::Mat image;
+  return 0;
+}
