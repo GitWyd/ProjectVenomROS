ab1311203d367d2f96381e56de82e8febc359604
YanS0ng <c.yansong@columbia.edu>
2018-04-19 11:21:38 -0400

    feat: Load data for trajectory prediction
    Example:
    $ rosrun venom_perception traj_predictor -d data/

---
+++ venom_perception/src/venom_perception/TrajPredict.py
@@ -0,0 +1,50 @@
+import glob
+import numpy as np
+
+def rolling_window(array, window, time_axis=0):
+    if time_axis == 0:
+        array = array.T
+
+    elif time_axis == -1:
+        pass
+
+    else:
+        raise ValueError('Time axis must be 0 (first dimension) or -1 (last)')
+
+    assert window >= 1, "`window` must be at least 1."
+    assert window < array.shape[-1], "`window` is too long."
+
+    # with strides
+    shape = array.shape[:-1] + (array.shape[-1] - window, window)
+    strides = array.strides + (array.strides[-1],)
+    arr = np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
+
+    if time_axis == 0:
+        return np.rollaxis(arr.T, 1, 0)
+    else:
+        return arr
+
+def _data_format(array, history):
+    x = rolling_window(array, history)
+    y = array[history:,:2]
+    return x, y
+
+def load_data(path, history, shuffle=True):
+    """ Read and reshape data
+    Keyword arguments:
+    path -- data directory
+    history -- number of previous steps
+    shuffle -- shuffle videos (not frames)
+    """
+    npyfiles = glob.glob(path+'*.npy')
+    data = []
+    for npyfile in npyfiles:
+        data.append(np.load(npyfiles[0]))
+
+    # NOTE: Must shuffle before concatenate, because rolling window works on a 
+    #       time series.
+    if shuffle:
+        np.random.shuffle(data)
+    data = np.concatenate(data,axis=0)
+    X, y = _data_format(data,history)
+    return X, y
